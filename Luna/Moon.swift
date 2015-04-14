1d769f00d6eb93152aba49bce39635e21ff060c4
Andrew Shepard <shep.andy@gmail.com>
2015-04-14 15:59:36 -0700

    Update Moon JSON parsing

--- Luna/Moon.swift
+++ Luna/Moon.swift
@@ -33,16 +33,16 @@ extension Moon {
         
         if let response = json["response"] as? [JSON],
             moon = response.first?["moon"] as? JSON,
-//            riseInterval = moon["rise"] as? NSTimeInterval,
-//            setInterval = moon["set"] as? NSTimeInterval,
+            riseInterval = moon["rise"] as? NSTimeInterval,
+            setInterval = moon["set"] as? NSTimeInterval,
             phase = moon["phase"] as? JSON,
             phaseName = phase["name"] as? String,
             age = phase["age"] as? Double,
             percent = phase["phase"] as? Double,
             illum = phase["illum"] as? Int {
                 
-                let rise = NSDate(timeIntervalSince1970: 0)
-                let set = NSDate(timeIntervalSince1970: 0)
+                let rise = NSDate(timeIntervalSince1970: riseInterval)
+                let set = NSDate(timeIntervalSince1970: setInterval)
                 return Moon(phaseName, age, percent, illum, rise, set)
         }
         
