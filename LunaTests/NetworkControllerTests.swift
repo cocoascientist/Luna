54d918971f4823149e1e486badf1d30de6ed0f0c
Andrew Shepard <shep.andy@gmail.com>
2016-03-22 09:02:49 -0700

    Implement location error test

--- LunaTests/NetworkControllerTests.swift
+++ LunaTests/NetworkControllerTests.swift
@@ -18,16 +18,6 @@ class NetworkControllerTests: XCTestCase {
         return location
     }
 
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-
     func testCanRequestMoonSuccessfully() {
         let expectation = expectationWithDescription("Request should be successful")
         let configuration = NSURLSessionConfiguration.configurationWithProtocol(LocalURLProtocol)
