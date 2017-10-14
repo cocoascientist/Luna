38b32ec81cae0e1ec42d2cf89ee65258f140a1c2
Andrew Shepard <shep.andy@gmail.com>
2017-10-14 11:48:39 -0400

    Use new api token

--- Luna/Network/AerisAPI.swift
+++ Luna/Network/AerisAPI.swift
@@ -50,10 +50,10 @@ enum AerisAPI: Request {
 
 extension AerisAPI {
     fileprivate var clientSecret: String {
-        return "\u{0035}\u{0038}w669fct\u{0070}2OYs4xCO1ty2OFyZ1xU56eHETe6h8V"
+        return "58w669fctp2OYs4xCO1ty2OFyZ1xU56eHETe6h8V"
     }
     
     fileprivate var clientId: String {
-        return "\u{0071}\u{004F}0Mw\u{0072}GSnfBKizM4B9Bt7"
+        return "qO0MwrGSnfBKizM4B9Bt7"
     }
 }
